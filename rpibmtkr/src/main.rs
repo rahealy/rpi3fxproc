a7befc8e7454f3aa4a2377bec8e07113642e7963
Richard A. Healy <rahealy@users.noreply.github.com>
2019-10-14 15:47:55 -0500

    Misc. updates.

--- rpibmtkr/src/main.rs
+++ rpibmtkr/src/main.rs
@@ -2,9 +2,6 @@
 #![no_main]
 
 use core::panic::PanicInfo;
-//use peripherals::{i2c, mbox, uart};
-use peripherals::PERIPHERALS;
-use hats::ultra2;
 
 mod startup; //Pull in startup code.
 
@@ -24,17 +21,6 @@ fn panic(_info: &PanicInfo) -> ! { loop {} }
 ///
 #[export_name = "main"] //So startup.rs can find fn main().
 fn main() -> ! {
-    let mut ultra2 = ultra2::Ultra2::new();
-
-    PERIPHERALS.init();
-
-    if let Err(err) = ultra2.init() {
-        PERIPHERALS.uart.puts("main(): Error ultra2.init() failed - ");        
-        PERIPHERALS.uart.puts(err.msg());
-    } else {
-        PERIPHERALS.uart.puts("UART Initialized.\r\n");
-    }
-
     loop {
 //Hello world.
     }
